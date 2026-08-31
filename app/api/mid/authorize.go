88b5b6f7fd9e6b2adef219166b7169f7a0f77842
rajuhuptaooooo <rajuhuptaooooo@gmail.com>
2026-08-31 19:17:07 +0530

    typo-mistakes

---
+++ app/api/mid/authorize.go
@@ -0,0 +1,27 @@
+package mid
+
+import (
+	"context"
+	"errors"
+
+	"github.com/ardanlabs/service/app/api/errs"
+	"github.com/ardanlabs/service/business/api/auth"
+	"github.com/ardanlabs/service/foundation/logger"
+)
+
+// ErrInvalidID represents a condition where the id is not a uuid.
+var ErrInvalidID = errors.New("ID is not in its proper form")
+
+// Authorize executes the specified role and does not extract any domain data
+func Authorize(ctx context.Context, log *logger.Logger, auth *auth.Auth, rule string, handler Handler) error {
+	userID, err := GetUserID(ctx)
+	if err != nil {
+		return errs.New(errs.Unauthenticated, err)
+	}
+
+	if err := auth.Authorize(ctx, GetClaims(ctx), userID, rule); err != nil {
+		return errs.Newf(errs.Unauthenticated, "authorize: you  are not authorized for that action, claims[%v] rule[%v]: %s", GetClaims(ctx).Roles, rule, err)
+	}
+
+	return handler(ctx)
+}
