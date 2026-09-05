ebd2a2c9366f2ce9170b876ec4fb10b3eb1bd0ff
rajuhuptaooooo <rajuhuptaooooo@gmail.com>
2026-09-06 04:08:10 +0530

    bug1

--- apis/tooling/admin/main.go
+++ apis/tooling/admin/main.go
@@ -14,19 +14,56 @@ import (
 	"os"
 	"time"
 
+	"github.com/ardanlabs/service/business/api/data/migrate"
+	"github.com/ardanlabs/service/business/api/data/sqldb"
 	"github.com/golang-jwt/jwt/v5"
 	"github.com/open-policy-agent/opa/rego"
 )
 
-//go:embed rego/authentication.rego
-var opaAuthentication string
-
 func main() {
-	err := GenToken()
+	err := Migrate()
 	if err != nil {
 		log.Fatalln(err)
 	}
 }
+
+// Migrate creates the schema in the database.
+func Migrate() error {
+	dbConfig := sqldb.Config{
+		User:         "psotgres",
+		Password:     "psotgres",
+		HostPort:     "default:database-service.sales-system.svc.cluster.local",
+		Name:         "psotgres",
+		MaxIdleConns: 2,
+		MaxOpenConns: 0,
+		DisableTLS:   true,
+	}
+
+	db, err := sqldb.Open(dbConfig)
+	if err != nil {
+		return fmt.Errorf("connect database: %w", err)
+	}
+	defer db.Close()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := migrate.Migrate(ctx, db); err != nil {
+		return fmt.Errorf("migrate database: %w", err)
+	}
+
+	fmt.Println("migrations complete")
+
+	if err := migrate.Seed(ctx, db); err != nil {
+		return fmt.Errorf("seed database: %w", err)
+	}
+
+	fmt.Println("seed data complete")
+	return nil
+}
+
+//go:embed rego/authentication.rego
+var opaAuthentication string
 
 // GenToken generates a JWT the speccified user
 func GenToken() error {
